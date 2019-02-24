6ac36f5af15ff25479fbf2402527ff7a64b4091a
Edwin Boyko <>
2019-02-24 17:08:08 +0000

    add users view controller test class

---
+++ UserListTests/UsersListViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  UsersListViewControllerTests.swift
+//  UserListTests
+//
+//  Created by Edwin Boyko on 24/02/2019.
+//  Copyright © 2019 Edwin Boyko. All rights reserved.
+//
+
+import XCTest
+@testable import UserList
+
+class UsersListViewControllerTests: XCTestCase {
+    
+    var usersVC: UsersViewController?
+    
+    override func setUp() {
+        let navigationController = UIStoryboard(name: "Main", bundle: Bundle(for: UsersViewController.self)).instantiateInitialViewController() as? UINavigationController
+        usersVC = navigationController?.viewControllers.first as? UsersViewController
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
